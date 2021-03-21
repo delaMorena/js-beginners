064d98d68468177709b9ae399764877697d219bf
delaMorena <martadmdo@gmail.com>
2021-03-21 17:32:51 +0000

    just started

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,9 @@
 var age = prompt('What is your age?');
 
 // Your code below:
+function addTenYears(age){
+    ageToNumber = parseInt(age)
+    return ageToNumber + 10
+};
+
+console.log(addTenYears(age));
