0e9b6a166321a114a6510cbf51037256983c65c2
delaMorena <martadmdo@gmail.com>
2021-03-21 21:58:59 +0000

    another try

--- exercises/11-Create-a-new-function/app.js
+++ exercises/11-Create-a-new-function/app.js
@@ -1,3 +1,8 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
 // Declare your function here
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+console.log(getRandomInt(0,9))
--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,15 +1,30 @@
-function getColor(selection)
-{
-	switch(selection){
-		// Add more options here
+function getColor(selection){
+    selectionToLowerCase = selection.toLowerCase()
+    console.log("dentro de la fn getColor", selectionToLowerCase)
+	switch(selectionToLowerCase){
+		case ('red'):
+            console.log("entro y leo");
+            return true;
+            break;
+		case "blue":
+            return true;
+            break;
+		case "green":
+            return true;
+            break;
 	    default:
-	    	return false;  //returns false because the user picked an unavailable color
-	    break;               
+            console.log("estoy en el default");
+            
+            return false;  //returns false because the user picked an unavailable color  
+                       
 	}
 }
 
-var colorname = prompt('What color do you want?');
+var colorname = window.prompt('What color do you want?');
+console.log("llego despues del prompt", colorname);
+
 var isAvailable = getColor(colorname);
+console.log("llego despues de llamar a getColor", colorname, isAvailable);
 
 if(isAvailable)
 	console.log('Good news! That color is available');
