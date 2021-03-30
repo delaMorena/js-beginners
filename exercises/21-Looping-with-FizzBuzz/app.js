742cd5b9e1d39f688fcae1eff9c5f0652af472a8
delaMorena <martadmdo@gmail.com>
2021-03-30 11:34:47 +0000

    21 done

--- exercises/21-Looping-with-FizzBuzz/app.js
+++ exercises/21-Looping-with-FizzBuzz/app.js
@@ -1,18 +1,17 @@
 function fizzBuzz() {  
     
-	for (let i = 1; i >= 100; i++) {
+	for (let i = 1; i <= 100; i++) {
         if (i%3 == 0) {
-            console.log('Fizz');
-            
-            return 'Fizz';       
+            console.log('Fizz');      
         }else if (i % 5 == 0) {
-            return 'Buzz';
+            console.log("Buzz");
         }else if (i % 5 && i % 3 == 0) {
-            return 'FizzBuzz'
+            console.log("FizzBuzz");
         }else 
-        return i
+        console.log(i);
     }
     
 }
 
 fizzBuzz();
+// for(i=0;i<100;)console.log((++i%3?'':'Fizz')+(i%5?'':'Buzz')||i)
